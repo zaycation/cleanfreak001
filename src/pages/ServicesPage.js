c45e60c99d2b078158a2afa686bb1057a02050ec
zaycationdev <isaiahthomas098@gmail.com>
2021-06-26 00:13:17 -0700

    updated services page with images

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+
+import all from "../images/allemployees.jpg";
+import ceo from "../images/ceo.jpg";
+import ceo2 from "../images/ceoworking1.jpg";
+import ceo3 from "../images/ceoworking2.jpg";
+import dust1 from "../images/dusting1.jpg";
+import dust2 from "../images/dusting2.jpg";
+import emp1 from "../images/employees1.jpg";
+import emp2 from "../images/employees2.jpg";
+import sweep from "../images/sweeping.jpg";
 
 import Services from "../components/services";
 import "../css/services.css";
@@ -8,6 +18,105 @@ const ServicesPage = () => {
   return (
     <Container fluid={true} className="services__bg">
       <Services />
+      <Row>
+        <Col>
+          <Image
+            className="my-3"
+            src={all}
+            rounded
+            width="350"
+            style={{ border: "2px solid #c19ccc" }}
+          />
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={ceo}
+              rounded
+              width="200"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={ceo2}
+              rounded
+              width="200"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={ceo3}
+              rounded
+              width="200"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={dust1}
+              rounded
+              width="200"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={dust2}
+              rounded
+              width="200"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={emp1}
+              rounded
+              width="200"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={emp2}
+              rounded
+              width="200"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+        <Col>
+          <div className="d-flex align-items-center justify-content-center">
+            <Image
+              className="my-3"
+              src={sweep}
+              rounded
+              width="315"
+              style={{ border: "2px solid #c19ccc" }}
+            />
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
